673581c46dabcfdf4e56bac9e3ad8a07d5a2c1a6
Icemaster67 <angiejoyaa16@gmail.com>
2026-01-04 17:19:19 -0500

    Create input_variable.py

---
+++ input_variable.py
@@ -0,0 +1,6 @@
+name = input("What is your name?:\n ")
+country = input("Which country do you love:\n")
+
+print(f"\nhello, {name}.\nThank you for choosing {country} as your favorite country\n")
+
+street = input("what is your street address")
