5e4d4c70fd28cb0cf73f1c32574e5c8693b9dc63
Icemaster67 <angiejoyaa16@gmail.com>
2026-01-18 17:24:08 -0500

    Create if.py

---
+++ if.py
@@ -0,0 +1,12 @@
+color = input("What is your favorite color?: ")
+
+if color == "red":
+    print("\nWhy do you like the color of DANGER??????")
+elif color =="turquoise":
+    print("\nThat's my favorite color too!")
+elif color =="white":
+    print("\nUmm.... that's plain but who am I to judge")
+elif color =="skyblue":
+    print("\nThat is the color of the sky.Good choice!!!")
+else:
+    print("\nNice choice!")
